f89239b2d67336d4e129da86aadf8681cfd3d1d4
haonanhu02-jpg <haonanhu02@gmail.com>
2026-08-13 15:05:03 +0800

    record R1 foundation evidence

--- src/rag_platform/modules/__init__.py
+++ src/rag_platform/modules/__init__.py
@@ -1,2 +1,1 @@
-"""Deep business modules (introduced from R1 onward)."""
-"""Framework-free deep-module interfaces."""
+"""Deep business modules (introduced from R1 onward).""" """Framework-free deep-module interfaces."""
